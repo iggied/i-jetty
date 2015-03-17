8f725f055cd28c02f297b207c3d6a361913f81e8
ignatius <ignatius.dmello@me.com>
2015-03-17 17:04:47 +0530

    Initial commit

---
+++ i-jetty-ui/build/generated/source/buildConfig/defaultFlavor/debug/org/mortbay/ijetty/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.mortbay.ijetty;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.mortbay.ijetty";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "defaultFlavor";
+  public static final int VERSION_CODE = 32;
+  public static final String VERSION_NAME = "";
+}
